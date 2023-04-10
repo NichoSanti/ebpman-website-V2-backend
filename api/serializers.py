d3fe3a7d7aa673fb9a439f75772b867d9449931f
NichoSantil <MiltonNSantiago@gmail.com>
2023-04-10 12:58:22 -0500

    fixed contact serializer class

--- api/serializers.py
+++ api/serializers.py
@@ -4,5 +4,5 @@ from api.models import Contact
 class ContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contact
-        fields = ['first_name', 'last_name', 'email_address', 'updated']
+        fields = ['first_name', 'last_name', 'email_address', 'message']
 
