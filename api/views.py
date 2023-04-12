173b3ebca34e3189f38daf68021504142e06dc4b
Milton N Santiago <MiltonNSantiago@gmail.com>
2023-04-11 19:28:04 -0500

    started work on POST functionality

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Contact
+from .serializers import ContactSerializer
 
 # Create your views here.
+@api_view(['POST'])
+def sendContact(request):
+    data = request.data
+    note = Note.objects.create(
+        body=data['body']
+    )
+    serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
