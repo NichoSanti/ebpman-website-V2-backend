306bbfcc54b0056f28bfe063ae1916f78ba20cdc
Milton N Santiago <MiltonNSantiago@gmail.com>
2023-04-16 12:50:40 -0500

    mailing

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.core.mail import send_mail
 from .models import Contact
 from .serializers import ContactSerializer
 
@@ -18,8 +19,27 @@ def createContact(request):
     serializer = ContactSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
+
+        name = serializer.validated_data['name']
+        email = serializer.validated_data['email']
+        message = serializer.validated_data['message']
+
+        send_email(name, email, message)
+
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def send_email(name, email, message):
+    subject = 'New contact form submission'
+    from_email = 'your_email@example.com' # Replace with your own email
+    to_email = 'recipient@example.com' # Replace with the recipient's email
+    body = f"Name: {name}\nEmail: {email}\nMessage: {message}"
+
+    send_mail(subject, body, from_email, [to_email], fail_silently=False)
+
+
+
 
 @api_view(['GET'])
 def getContacts(request):
