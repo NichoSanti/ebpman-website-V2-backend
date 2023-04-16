5ec7ce119b55ea2e45cdc3e5d881a375dd909532
Milton N Santiago <MiltonNSantiago@gmail.com>
2023-04-16 14:10:40 -0500

    email functionality working

--- api/views.py
+++ api/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
-from django.http import JsonResponse
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.core.mail import send_mail
 from .models import Contact
 from .serializers import ContactSerializer
+from django.conf import settings
 
 
 @api_view(['GET'])
@@ -20,21 +19,22 @@ def createContact(request):
     if serializer.is_valid():
         serializer.save()
 
-        name = serializer.validated_data['name']
-        email = serializer.validated_data['email']
+        first_name = serializer.validated_data['first_name']
+        last_name = serializer._validated_data['last_name']
+        email = serializer.validated_data['email_address']
         message = serializer.validated_data['message']
 
-        send_email(name, email, message)
+        send_email(first_name, last_name, email, message)
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-def send_email(name, email, message):
+def send_email(first_name, last_name, email, message):
     subject = 'New contact form submission'
     from_email = 'your_email@example.com' # Replace with your own email
-    to_email = 'recipient@example.com' # Replace with the recipient's email
-    body = f"Name: {name}\nEmail: {email}\nMessage: {message}"
+    to_email = settings.DEFAULT_FROM_EMAIL # Replace with the recipient's email
+    body = f"Name: {first_name} {last_name}\nEmail: {email}\nMessage: {message}"
 
     send_mail(subject, body, from_email, [to_email], fail_silently=False)
 
